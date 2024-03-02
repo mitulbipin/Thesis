7f1bdd8fcc4a55dd8500f1f95c30aa7c857bdd66
mitulbipin <mitulbipin24@gmail.com>
2024-03-02 22:37:04 +0100

    Added repair_regex.php and renamed the regex sample file

--- repair_regex.php
+++ repair_regex.php
@@ -10,15 +10,15 @@
     if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
         $input = $_POST["input"];
-        $pattern = '/A(B|C+)+D/';
-       
+        //$pattern = '/A(B|C+)+D/';
+        $pattern = '/A((?:(?:(?!.).)|(?:(?:[BC])+)))D/';
         if (preg_match($pattern, $input)){
             echo "<p>Input is valid: $input</p>";
         } 
         else {
             echo "<p>Invalid input.</p>";
             header("HTTP/1.1 400 Bad Request");
-            }      
+            }
         }
     
     ?>
